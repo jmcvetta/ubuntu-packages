3d3f962442a2b77a3022ac740c3a194a3103e73f
Jason McVetta <jason.mcvetta@gmail.com>
2017-09-06 17:39:49 +0700

    install nodejs and yarn from PPAs

--- install.sh
+++ install.sh
@@ -5,10 +5,27 @@
 #
 #===============================================================================
 
+# Upgrade all packages
 apt-get update &&
-
 apt-get dist-upgrade -y &&
 
-apt-get install -y gdebi &&
+
+# Required to add PPAs
+apt-get install -y \
+    apt-transport-https \
+    apt-utils \
+    curl \
+    gdebi \
+    &&
+
+
+# Yarn PPA
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+
+# Node.js PPA
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+
 
 gdebi -n jmcvetta-workstation_*.deb
