02989f7b15a27ac4344bafade8c4570d0cd684f6
Jason McVetta <jason.mcvetta@gmail.com>
2018-08-28 06:08:14 -0400

    do not install nodejs stuff

--- install.sh
+++ install.sh
@@ -17,15 +17,6 @@ apt-get install -y \
     gdebi
 
 
-# Yarn PPA
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-
-
-# Node.js PPA
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-
-
 gdebi -n jmcvetta-workstation_*.deb
 
 apt-get dist-upgrade -y
