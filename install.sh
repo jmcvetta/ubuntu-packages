aea61646881c7f5dbf8bc877e92a03243325c7bd
Jason McVetta <jason.mcvetta@gmail.com>
2022-08-11 01:20:03 -0400

    update for ubuntu 22.04

--- install.sh
+++ install.sh
@@ -35,8 +35,6 @@ sudo apt install -y \
     bison \
     build-essential \
     byobu \
-    bzr \
-    calibre \
     chromium-browser \
     colordiff \
     curl \
@@ -51,13 +49,10 @@ sudo apt install -y \
     fortunes-spam \
     fortunes-ubuntu-server \
     gcc \
-    gimp \
     git \
     git-flow \
-    git-svn \
     glabels \
     gnome-gmail \
-    gnome-tweaks \
     gnome-web-photo \
     gparted \
     haskell-platform \
@@ -72,21 +67,19 @@ sudo apt install -y \
     libpcre3-dev \
     libpq-dev \
     libreadline-dev \
-    libreadline6-dev \
     libssl-dev \
     libtinfo-dev \
     libyaml-dev \
     llvm \
     llvm-dev \
     maven \
-    mercurial \
     openjdk-17-jdk \
     openvpn \
     p7zip-full \
     p7zip-rar \
     postgresql-client \
     pwgen \
-    python-dev \
+    python3-dev \
     python3-ipython \
     rake \
     ruby \
@@ -94,11 +87,8 @@ sudo apt install -y \
     ruby-dev \
     sshuttle \
     subversion \
-    texlive-latex-base \
     traceroute \
-    vagrant \
     vim \
-    virtualbox \
     virtualenvwrapper \
     whois \
     xdot \
