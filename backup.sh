b11249e736359252f67019211912189a95c93199
Jason McVetta <jmcvetta@hey.com>
2025-06-20 14:33:42 +0500

    accept RESTIC_PASSWORD and exclude more folders

--- backup.sh
+++ backup.sh
@@ -7,21 +7,27 @@
 
 set -x
 
-sudo restic \
+sudo RESTIC_PASSWORD="$RESTIC_PASSWORD" restic \
 	-r ./jmcvetta-backup \
 	-e **/*Cache* \
 	-e **/*cache* \
+	-e .BurpSuite \
 	-e .asdf \
 	-e .cache \
 	-e .cargo \
+	-e .config/GIMP \
+	-e .config/google-chrome \
 	-e .local \
+	-e .m2 \
 	-e .npm \
 	-e .nuget \
+	-e .ollama \
 	-e .paradoxlauncher \
 	-e .pulumi \
 	-e .rustup \
 	-e .rye \
 	-e .yor_plugins \
+	-e Downloads \
 	-e bin \
 	-e go \
 	-e jan \
